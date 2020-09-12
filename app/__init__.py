f20a6fd203f98c81baeb6c32d105a5ebbfc0e4d6
Brian Tinega Nyang'wara <briantinega@gmail.com>
2020-09-12 15:05:32 +0300

    setting up configurations and deleting arg:debug =True in run.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,10 @@
 from flask import Flask
+from .config import DevConfig
 
 #initializing application
 app = Flask(__name__)
 from app import views
+
+# Setting up configuration
+app.config.from_object(DevConfig)
+
