95b10eeb00ac7a27cff0e76d247c5bb64d4ee251
Brian Tinega Nyang'wara <briantinega@gmail.com>
2020-09-12 16:39:48 +0300

    get apiKey and base_url to requests.py

--- app/request.py
+++ app/request.py
@@ -2,3 +2,10 @@ from app import app
 import urllib.request,json
 from .models import news
 Article = news.Article
+
+#Getting api-Key
+apiKey= app.config['News_Api_Key']
+
+#Getting the news base-url
+
+base_url=app.config['Top_Headlines_Url']
