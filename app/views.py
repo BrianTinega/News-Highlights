206ed0bbc7b8f71cd1a616b61520f2df5d400d56
Brian Tinega Nyang'wara <briantinega@gmail.com>
2020-09-13 19:33:27 +0300

    add macro for displaying sources

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from app import app
 from flask import render_template
-from .request import get_news
+from .request import get_news,get_sources
 
 @app.route('/')
 def index():
@@ -8,4 +8,6 @@ def index():
     print(top)
     sports_news = get_news()
     print(sports_news)
-    return render_template('index.html', headlines = top, sports = sports_news)
+    news_sources = get_sources()
+    print(news_sources)
+    return render_template('index.html', headlines = top, sports = sports_news, root_sources = news_sources)
