7d974aa412b500d6c8c9d8b1162d1f33f050a119
Brian Tinega Nyang'wara <briantinega@gmail.com>
2020-09-14 11:33:03 +0300

    add trump url and germany url

--- app/config.py
+++ app/config.py
@@ -5,8 +5,9 @@ class Config:
     '''
     TOP_HEADLINES_URL='https://newsapi.org/v2/top-headlines?country=us&apiKey={}'
     SOURCES_URL='https://newsapi.org/v2/sources?apiKey={}'
-
-
+    TRUMP_URL = "https://newsapi.org/v2/top-headlines?q=trump&apiKey={}"
+    GERMANY_URL="https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey={}"
+    
 
 class ProdConfig(Config):
     '''
